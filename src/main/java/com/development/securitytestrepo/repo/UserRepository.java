243d558e8cf339d6dc15445e9771b1730e5e6bc7
AnonimProgrammer <zephyr11ff@gmail.com>
2025-09-08 16:41:45 +0400

    User entity is designed. Repo is added.

---
+++ src/main/java/com/development/securitytestrepo/repo/UserRepository.java
@@ -0,0 +1,11 @@
+package com.development.securitytestrepo.repo;
+
+import com.development.securitytestrepo.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+}
