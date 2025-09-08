be276c73510150484593fd4096eb112ab72c2adf
AnonimProgrammer <zephyr11ff@gmail.com>
2025-09-08 16:41:25 +0400

    custom user details service is implemented.

---
+++ src/main/java/com/development/securitytestrepo/service/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.development.securitytestrepo.service;
+
+import com.development.securitytestrepo.exception.UserNotFoundException;
+import com.development.securitytestrepo.model.User;
+import com.development.securitytestrepo.model.UserPrincipal;
+import com.development.securitytestrepo.repo.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Username not found."));
+
+        return new UserPrincipal(user);
+    }
+}
