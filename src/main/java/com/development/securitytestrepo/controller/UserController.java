867dc55936ba45e860227f6b1cd5f1488fbd55c8
AnonimProgrammer <zephyr11ff@gmail.com>
2025-09-09 23:16:11 +0400

    register and login endpoints are added

---
+++ src/main/java/com/development/securitytestrepo/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.development.securitytestrepo.controller;
+
+import com.development.securitytestrepo.model.User;
+import com.development.securitytestrepo.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public User registerUser(@RequestBody User user) {
+        return userService.registerUser(user);
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody User user) {
+        return userService.verifyUser(user);
+    }
+}
